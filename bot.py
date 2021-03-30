663da82d566f7ef37ae806a150a0009813df2575
BuildTools <chrisconcord@gmail.com>
2021-03-30 04:25:50 -0400

    Implemented user 'accounts' with mongodb, as well as a balance system. Also brought in the LoL API code from previous project

--- bot.py
+++ bot.py
@@ -4,16 +4,26 @@ from dotenv import load_dotenv
 from discord.ext import commands
 import pymongo
 from pymongo import MongoClient
+from utils.panda import *
+import random
 
+#discord setup
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-
 client = discord.Client()
-bot = commands.Bot(command_prefix='!!')
+intents = discord.Intents.default()
+intents.members = True
+bot = commands.Bot(command_prefix=commands.when_mentioned_or('!!'), intents=intents)
 
-# @client.event
-# async def on_ready():
-#     print(f'{client.user} has connected to discord')
+
+#mongo
+pw = os.getenv('MONGODB_PASSWORD')
+cluster = pymongo.MongoClient("mongodb+srv://Chris:" + str(pw) + "@yupbot.jim1g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+db = cluster["YupBot"]
+collection = db["UserData"]
+
+#panda setup
+PANDA = PandaAPI()
 
 @client.event
 async def on_error(event, *args, **kwargs):
@@ -21,13 +31,153 @@ async def on_error(event, *args, **kwargs):
         if event == 'on_message':
             f.write('Unhandled message: {args[0]}\n')
         else:
-            raise
+            f.write('Unknown Error')
 
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to discord!')
 
+# commands
+@bot.command(name='register', help='For first time users, let\'s set you up with a bank account so you can gamble it all away!')
+async def register(ctx):
+    __id = ctx.author.id
+    __mention = ctx.author.mention
+    __name = ctx.author.name
+    is_registered = collection.find_one({"_id": __id})
+    if is_registered:
+        await ctx.channel.send('You are already registered ' + str(__mention))
+        return
+    # else
+    post = {"_id": __id, "balance": 0, "name":__name}
+    collection.insert_one(post)
+    await ctx.channel.send('Congratulations ' + __mention + ', you have been registered!')
 
+@bot.command(name='balance', help='Check your current balance. Can also check others\' balances, usage: \"!!balance @[user]')
+async def check_balance(ctx, *args):
+    if len(args) == 0:
+        __id = ctx.author.id
+        member = ctx.author
+    else:
+        mention = args[0]
+        mention = mention.replace("<","")
+        mention = mention.replace(">","")
+        mention = mention.replace("@","")
+        mention = mention.replace("!","")
+        __id = int(mention)
+        member = ctx.guild.get_member(__id)
+        if member == None:
+            await ctx.channel.send("User not found, did you make a typo?")
+            return
+    query = {"_id":__id}
+    doc = collection.find_one(query)
+    if doc:
+        balance = doc["balance"]
+        message = "The user " + str(member.mention) + " currently has a balance of $" + str(balance)
+    else:
+        message = "The user " + str(member.mention) + " is not registered yet. Register using: \"!!register\""
+    await ctx.channel.send(message)
+
+@bot.command(name='test', help='For debugging purposes')
+async def test(ctx, *args):
+    if len(args) > 0:
+        arg1 = args[0]
+        if len(args) > 1:
+            arg2 = args[1]
+            if arg1 == "setbalance":
+                query = {"_id": ctx.author.id}
+                update = {"$set": {"balance": arg2}}
+                collection.update_one(query, update)
+    author = ctx.author
+    member = ctx.guild.get_member(author.id)
+    print("id:", author.id)
+    print("member:", member)
+
+@bot.command(name='gamble', help='This is what you came here for.\nUsage: \"!!gamble [amount] ...\"')
+async def gamble(ctx, amount, *args):
+    message = "error"
+    author = ctx.author
+    amount = int(amount)
+    query = {"_id": author.id}
+    doc = collection.find_one(query)
+    if doc:
+        balance = int(doc["balance"])
+        if balance < amount:
+            message = str(author.mention) + " You lack the critical funds! Get a job!"
+        else:
+            if len(args) == 0: # coin flip
+                odds = 0.5
+                rand = random.random()
+                if rand > odds: # win
+                    message = str(author.mention) + " Congratulations! You won $" + str(amount)
+                    update = {"$set": {"balance": balance + amount}}
+                    collection.update_one(query, update)
+                else:   #lose
+                    message = str(author.mention) + " Unlucky, better luck next time, you lost all $" + str(amount)
+                    update = {"$set": {"balance": balance - amount}}
+                    collection.update_one(query, update)
+            else:
+                command = args[0]
+                if command == "dice":
+                    if len(args) > 2:
+                        sides = int(args[1])
+                        prediction = int(args[2])
+                        outcome = random.randint(1,sides)
+                        if prediction > sides:
+                            message = "Thats not how dice work...\n!!gamble [amount] dice [number of sides] [prediction]"
+                        else:
+                            if prediction == outcome:   # win
+                                winnings = amount * sides
+                                message = "Outcome was " + str(outcome) + ", you guessed " + str(prediction) +"\n" + str(author.mention) + " Congratulations! You won $" + str(winnings - amount)
+                                update = {"$set": {"balance": balance - amount + winnings}}
+                                collection.update_one(query, update)
+                            else:   # loss
+                                message = "Outcome was " + str(outcome) + ", you guessed " + str(prediction) +"\n" + str(author.mention) + " Never lucky! You lost $" + str(amount)
+                                update = {"$set": {"balance": balance - amount}}
+                                collection.update_one(query, update)
+                    else:
+                        message = "!!gamble [amount] dice [number of sides] [prediction]"
+                else:
+                    message = "Invalid command"
+    else:
+        message = str(author.mention) + " You are not registered yet. Register using: \"!!register\""
+    await ctx.channel.send(message)
+
+@bot.command(name='kait', help='You know what this does...')
+async def kait(ctx):
+    if random.random() > 0.5:
+        await ctx.channel.send("kait: I'll be back")
+    else:
+        await ctx.channel.send("kait: Hello???")
+    
+@bot.command(name='lol', help='Many League of Legends commands')
+async def test(ctx, command, *args):
+    embed = None
+   
+    if command == 'icon':    # url to champion icon
+        if len(args) == 0:
+            message = "Usage: \"!!lol icon [champion_name]\""
+        else:
+            __name = args[0]
+            url = PANDA.get_champ_portrait(__name)
+            if url:
+                message = url
+            else:
+                message = "Error, maybe you made a typo"
+    elif command == 'schedule': # upcoming week's schedule
+        schedule_string = PANDA.get_schedule_message(str(PANDA.LCS_ID))
+        embed = discord.Embed(
+            title="This week's schedule",
+            url="https://lolesports.com/schedule" 
+        )
+        embed.add_field(name="Matches:", value = schedule_string)
+        if schedule_string:
+            message = None
+        else:
+            message = "Error, something went wrong"
+    else:   # does not match any commands
+        message = "Invalid command"
+
+    await ctx.channel.send(message, embed=embed)
 
 
 bot.run(TOKEN)
---
+++ utils/panda.py
@@ -0,0 +1,92 @@
+import os
+import requests
+import json
+import datetime
+from datetime import timedelta
+
+class PandaAPI:
+    def __init__(self):
+        self.base_url = 'https://api.pandascore.co'
+        self.token = os.getenv('PANDA_TOKEN')
+        self.auth = '?token=' + self.token
+        self.LCS_ID = 4198
+        self.ONE_WEEK = timedelta(days=7)
+        self.GMT_TO_EST = timedelta(hours=5)
+
+    def query_panda(self, path, parameters):
+        response = requests.get(self.base_url + path + self.auth + parameters)
+        return json.loads(response.text)
+    def get_champs(self):
+        # response = requests.get("https://api.pandascore.co/lol/champions?token=" + os.getenv('PANDA'))
+        # json_data = json.loads(response.text)
+        # return json_data
+        path = "/lol/champions"
+        parameters = "&page[size]=1"
+        return self.query_panda(path,parameters)
+
+    # gets champion data by name 
+    def get_champ(self, name):
+        path = "/lol/champions"
+        parameters = "&search[name]=" + name
+        return self.query_panda(path,parameters)
+
+    # gets champ icon url by name
+    def get_champ_portrait(self, name):
+        champ = self.get_champ(name)
+        if len(champ) > 0:
+            return champ[0]["image_url"]
+        return None
+
+    # gets a list of all the leagues from panda api
+    def get_leagues(self):
+        path = "/lol/leagues"
+        parameters = ""
+        return self.query_panda(path,parameters)
+    def get_matches(self, league_id_string):
+        parameters = "&filter[league_id]=" + league_id_string
+        path = "/lol/matches"
+        return self.query_panda(path,parameters)
+    # gets 
+    def get_upcoming_matches(self, league_id_string):
+        parameters = "&filter[league_id]="+league_id_string+"&sort=begin_at"
+        path = "/lol/matches/upcoming"
+        return self.query_panda(path,parameters)
+    # returns array of match objects within the next week
+    def get_matches_within_week(self, league_id_string):
+        now = datetime.datetime.now()
+        week_from_now = now + self.ONE_WEEK
+        matches = self.get_upcoming_matches(league_id_string)
+        for match in reversed(matches):
+            match_time = match["begin_at"]
+            match_time = datetime.datetime.strptime(match_time, "%Y-%m-%dT%H:%M:%SZ")
+            if match_time < week_from_now:
+                break
+            matches.pop()
+        return matches
+    def match_to_schedule_string(self, match, utc_offset):
+        utc_offset = timedelta(hours=utc_offset)
+        team1 = match["opponents"][0]["opponent"]["acronym"]
+        team2 = match["opponents"][1]["opponent"]["acronym"]
+        matchup = team1 + " vs " + team2
+        match_time = match["begin_at"]
+        match_time = datetime.datetime.strptime(match_time, "%Y-%m-%dT%H:%M:%SZ") - utc_offset
+        date_string = match_time.strftime("%x")
+        time_string = match_time.strftime("%I:%M%p")
+        day_of_week = match_time.strftime("%A")
+        day = match_time.strftime('%d')
+        if day[0] == '0':
+            day = day[1]
+        month = match_time.strftime('%m')
+        if month[0] == '0':
+            month = month[1]
+        full_string = matchup + " at " + time_string + " on " + day_of_week + " " + month + "/" + day
+        return full_string
+    def generate_schedule_message(self, matches, utc_offset):
+        message = ""
+        for match in matches:
+            message += self.match_to_schedule_string(match,5) + "\n"
+        return message
+    def get_schedule_message(self, league_id_string):
+        matches = self.get_matches_within_week(league_id_string)
+        message = self.generate_schedule_message(matches,5)
+        return message
